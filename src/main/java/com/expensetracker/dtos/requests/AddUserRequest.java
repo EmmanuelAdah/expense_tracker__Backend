7a9b34e608fc6d4c50d8de0ec823b6e2848f4741
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-23 17:08:49 +0100

    for user request fields upon user registration

---
+++ src/main/java/com/expensetracker/dtos/requests/AddUserRequest.java
@@ -0,0 +1,14 @@
+package com.expensetracker.dtos.requests;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class AddUserRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String username;
+    private String password;
+}
