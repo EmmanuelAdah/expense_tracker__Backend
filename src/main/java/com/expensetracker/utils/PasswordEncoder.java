62dd63c5d4f0c0c170fd5dff74a2025d0af2af84
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-23 17:13:52 +0100

    for password hashing and checking

---
+++ src/main/java/com/expensetracker/utils/PasswordEncoder.java
@@ -0,0 +1,14 @@
+package com.expensetracker.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordEncoder {
+
+    public static String hashPassword(String password){
+        return BCrypt.hashpw(password, BCrypt.gensalt(8));
+    }
+
+    public static boolean checkPassword(String password, String hashedPassword){
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+}
