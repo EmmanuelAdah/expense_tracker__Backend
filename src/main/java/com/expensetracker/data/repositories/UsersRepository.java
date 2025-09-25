673bf0980040ee1f56e39e560654defb8fc132da
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-25 08:52:00 +0100

    users repository for manipulating the database

--- src/main/java/com/expensetracker/data/repositories/UsersRepository.java
+++ src/main/java/com/expensetracker/data/repositories/UsersRepository.java
@@ -1,10 +1,10 @@
 package com.expensetracker.data.repositories;
 
 import com.expensetracker.data.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
 }
