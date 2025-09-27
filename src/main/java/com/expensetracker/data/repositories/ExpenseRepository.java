478a50b00d0bc85a9672b03b904bc30b4df3263a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-27 15:32:41 +0100

    for manipulating the expense database

---
+++ src/main/java/com/expensetracker/data/repositories/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.expensetracker.data.repositories;
+
+import com.expensetracker.data.models.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
