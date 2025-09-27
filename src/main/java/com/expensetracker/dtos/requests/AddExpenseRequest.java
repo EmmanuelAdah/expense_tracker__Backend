1e2d8654dac53832d5feae750fcd769c5c00a721
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-27 22:37:07 +0100

    add expense request fields

--- src/main/java/com/expensetracker/dtos/requests/AddExpenseRequest.java
+++ src/main/java/com/expensetracker/dtos/requests/AddExpenseRequest.java
@@ -2,17 +2,29 @@ package com.expensetracker.dtos.requests;
 
 
 import com.expensetracker.data.models.Type;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class AddExpenseRequest {
-    private String userId;
+
+    private long userId;
+
+    @NotBlank()
+    @Size(min = 3, max = 100, message = "Name must be between {min} and {max} letters long")
     private String name;
+
+    @NotNull
     private double amount;
+
     private String category;
     private Type type;
+
+
     private String dateAdded;
     private String dueDate;
 }
