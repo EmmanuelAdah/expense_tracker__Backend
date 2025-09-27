528494b175b6f74a39e14792269aba0e97897083
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-27 22:37:58 +0100

    add user response entity for its fields

--- src/main/java/com/expensetracker/dtos/response/UserResponse.java
+++ src/main/java/com/expensetracker/dtos/response/UserResponse.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 public class UserResponse {
-    private String userId;
+    private long userId;
     private String email;
     private String username;
 }
