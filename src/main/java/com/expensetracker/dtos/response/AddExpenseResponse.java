4b99e9e2bfd8994b639cb0da771061e16079d2ec
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-27 15:36:08 +0100

    for expense client response fields

---
+++ src/main/java/com/expensetracker/dtos/response/AddExpenseResponse.java
@@ -0,0 +1,17 @@
+package com.expensetracker.dtos.response;
+
+import com.expensetracker.data.models.Type;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class AddExpenseResponse {
+    private String id;
+    private String name;
+    private double amount;
+    private String category;
+    private Type type;
+    private String dateAdded;
+    private String dueDate;
+}
