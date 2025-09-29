b96a41a83cd27fab4cb2226eec806fbe3403e7cd
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-29 20:39:44 +0100

    user entity for its attributes

--- src/main/java/com/expensetracker/data/models/User.java
+++ src/main/java/com/expensetracker/data/models/User.java
@@ -1,13 +1,17 @@
 package com.expensetracker.data.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Entity
-@Table
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
