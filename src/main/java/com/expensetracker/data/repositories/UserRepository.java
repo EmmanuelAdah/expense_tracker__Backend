f290bf6ca799a1231df80f83e06ec7bbdd1533f6
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-01 23:04:48 +0100

    for manipulating users database

--- src/main/java/com/expensetracker/data/repositories/UserRepository.java
+++ src/main/java/com/expensetracker/data/repositories/UserRepository.java
@@ -1,14 +1,13 @@
 package com.expensetracker.data.repositories;
 
-import com.expensetracker.data.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
+import com.expensetracker.data.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     void deleteByUsername(String username);
 
