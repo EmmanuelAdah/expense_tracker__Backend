3db05bced677b502256495ab3167d3d1f3efc926
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-01 23:01:23 +0100

    for security configuration via spring security concepts

--- src/main/java/com/expensetracker/security/SecurityConfig.java
+++ src/main/java/com/expensetracker/security/SecurityConfig.java
@@ -1,22 +1,40 @@
 package com.expensetracker.security;
 
+import com.expensetracker.config.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.expensetracker.config.ApplicationConfig;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final ApplicationConfig applicationConfig;
+
+    // To filter http request upon client request
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf()
-                .disable();
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                auth.requestMatchers("/api/tracker/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(applicationConfig.authenticationProvider())
+                .addFilterBefore(jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
