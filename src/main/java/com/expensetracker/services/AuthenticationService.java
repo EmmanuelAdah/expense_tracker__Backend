f85253bc0d4fb4867e8d6547fa35b4965cc13a5f
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-01 23:07:11 +0100

    for rendering authentication services

---
+++ src/main/java/com/expensetracker/services/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.expensetracker.services;
+
+import com.expensetracker.config.JwtService;
+import com.expensetracker.data.models.User;
+import com.expensetracker.data.repositories.UserRepository;
+import com.expensetracker.dtos.requests.AuthenticationRequest;
+import com.expensetracker.dtos.requests.RegisterRequest;
+import com.expensetracker.dtos.response.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import static com.expensetracker.utils.PasswordEncoder.hashPassword;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .username(request.getUsername())
+                .password(hashPassword(request.getPassword()))
+                .build();
+
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getUsername(),
+                request.getPassword())
+        );
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException(request.getUsername() + " not found"));
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+}
