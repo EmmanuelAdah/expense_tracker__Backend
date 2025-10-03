fd45c6cd587f04a928bea8ed012db2dd0cf49883
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-03 18:39:13 +0100

    for client expense request and server response

--- src/main/java/com/expensetracker/controllers/ExpenseController.java
+++ src/main/java/com/expensetracker/controllers/ExpenseController.java
@@ -1,23 +1,32 @@
 package com.expensetracker.controllers;
 
 import com.expensetracker.dtos.requests.AddExpenseRequest;
-import com.expensetracker.dtos.response.AddExpenseResponse;
+import com.expensetracker.dtos.response.ExpenseResponse;
 import com.expensetracker.services.ExpenseServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ExpenseController {
     private final ExpenseServiceImpl expenseServiceImpl;
 
-    @Autowired
-    public ExpenseController(ExpenseServiceImpl expenseServiceImpl){
-        this.expenseServiceImpl = expenseServiceImpl;
+    @PostMapping
+    public ResponseEntity<ExpenseResponse> addExpense(AddExpenseRequest request){
+        return ResponseEntity.ok(expenseServiceImpl.saveExpense(request));
     }
 
-    @PostMapping
-    public AddExpenseResponse addExpense(AddExpenseRequest request){
-        return expenseServiceImpl.saveExpense(request);
+    @GetMapping
+    public ResponseEntity<List<ExpenseResponse>> findNotesByUserId(Long userId){
+        return ResponseEntity.ok(expenseServiceImpl.findByUserId(userId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ExpenseResponse>> findAll(){
+        return ResponseEntity.ok(expenseServiceImpl.findAll());
     }
 }
