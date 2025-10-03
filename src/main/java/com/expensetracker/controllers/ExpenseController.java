c0d84017da149efc95f94802f12df3f50edd562b
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-03 21:28:50 +0100

    for client expense request and server response

--- src/main/java/com/expensetracker/controllers/ExpenseController.java
+++ src/main/java/com/expensetracker/controllers/ExpenseController.java
@@ -7,25 +7,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/tracker/expenses")
 public class ExpenseController {
     private final ExpenseServiceImpl expenseServiceImpl;
 
-    @PostMapping
+    @PostMapping("/addExpense")
     public ResponseEntity<ExpenseResponse> addExpense(AddExpenseRequest request){
         return ResponseEntity.ok(expenseServiceImpl.saveExpense(request));
     }
 
-    @GetMapping
+    @GetMapping("/findNoteByUserId")
     public ResponseEntity<List<ExpenseResponse>> findNotesByUserId(Long userId){
         return ResponseEntity.ok(expenseServiceImpl.findByUserId(userId));
     }
 
-    @GetMapping
+    @GetMapping("/findAllNotes")
     public ResponseEntity<List<ExpenseResponse>> findAll(){
         return ResponseEntity.ok(expenseServiceImpl.findAll());
     }
