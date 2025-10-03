13f4bf07820fb4639de19e957e287d22ea57db87
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-03 18:40:01 +0100

    for server response upon client request

--- src/main/java/com/expensetracker/services/ExpenseService.java
+++ src/main/java/com/expensetracker/services/ExpenseService.java
@@ -1,11 +1,14 @@
 package com.expensetracker.services;
 
 import com.expensetracker.dtos.requests.AddExpenseRequest;
-import com.expensetracker.dtos.response.AddExpenseResponse;
+import com.expensetracker.dtos.response.ExpenseResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ExpenseService {
-
-    AddExpenseResponse saveExpense(AddExpenseRequest request);
+    ExpenseResponse saveExpense(AddExpenseRequest request);
+    List<ExpenseResponse> findByUserId(Long userId);
+    List<ExpenseResponse> findAll();
 }
