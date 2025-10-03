1bd2226923cb977f8eee243eba7f2a4fc9484886
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-03 21:27:42 +0100

    expense service implementation

--- src/main/java/com/expensetracker/services/ExpenseServiceImpl.java
+++ src/main/java/com/expensetracker/services/ExpenseServiceImpl.java
@@ -5,23 +5,27 @@ import com.expensetracker.data.models.User;
 import com.expensetracker.data.repositories.ExpenseRepository;
 import com.expensetracker.data.repositories.UserRepository;
 import com.expensetracker.dtos.requests.AddExpenseRequest;
-import com.expensetracker.dtos.response.AddExpenseResponse;
+import com.expensetracker.dtos.response.ExpenseResponse;
 import com.expensetracker.exceptions.UserNotFoundException;
+import com.expensetracker.utils.Mapper;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static com.expensetracker.utils.Mapper.map;
 import static com.expensetracker.utils.Mapper.mapExpense;
 
 @Service
+@RequiredArgsConstructor
 public class ExpenseServiceImpl implements ExpenseService{
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private ExpenseRepository expenseRepository;
+    private final UserRepository userRepository;
+    private final ExpenseRepository expenseRepository;
 
     @Override
-    public AddExpenseResponse saveExpense(AddExpenseRequest request) {
+    public ExpenseResponse saveExpense(AddExpenseRequest request) {
         User user = userRepository.findById(request.getUserId())
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
 
@@ -29,5 +33,24 @@ public class ExpenseServiceImpl implements ExpenseService{
         user.getExpenses().add(expense);
         userRepository.save(user);
         return map(expense);
+    }
+
+    @Override
+    public List<ExpenseResponse> findAll() {
+        return expenseRepository.findAll()
+                .stream()
+                .map(Mapper::map)
+                .toList();
+    }
+
+    @Override
+    public List<ExpenseResponse> findByUserId(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        return user.getExpenses()
+                .stream()
+                .map(Mapper::map)
+                .toList();
     }
 }
