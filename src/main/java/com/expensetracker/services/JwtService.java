f72ce33f9884ad85c4ac84abd11296117d21a02d
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-04 17:27:29 +0100

    Json web token services

--- src/main/java/com/expensetracker/services/JwtService.java
+++ src/main/java/com/expensetracker/services/JwtService.java
@@ -1,13 +1,13 @@
-package com.expensetracker.config;
+package com.expensetracker.services;
 
 import com.expensetracker.data.models.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
@@ -17,10 +17,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-
-    private final Key secretKey = Keys
-            .hmacShaKeyFor("06904e8dc8b08f33602356eb35f78f8fc32284d09688a199ce463be4692c9626"
-                    .getBytes());
+    private final String SECRET_KEY =
+            "06904e8dc8b08f33602356eb35f78f8fc32284d09688a199ce463be4692c9626";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -36,32 +34,32 @@ public class JwtService {
         Instant now = Instant.now();
 
         return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(user.getUsername())
-                .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(now.plus(Duration.ofHours(24))))
-                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .claims(claims)
+                .subject(user.getUsername())
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plus(Duration.ofHours(24))))
+                .signWith(generateSignInKey())
                 .compact();
     }
 
     //    To check if a token is valid
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()));
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     //    To check if a token is expired
-    public boolean isTokenExpired(String token){
+    private boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
 
     //    For extracting expiry date
-    public Date extractExpiration(String token) {
+    private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-
     //    To extract user details such as the username upon request
+
     public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(jwtToken);
         return claimsResolver.apply(claims);
@@ -71,9 +69,14 @@ public class JwtService {
     public Claims extractAllClaims(String jwtToken) {
         return Jwts
                 .parser()
-                .setSigningKey(secretKey)
+                .verifyWith(generateSignInKey())
                 .build()
-                .parseClaimsJws(jwtToken)
-                .getBody();
+                .parseSignedClaims(jwtToken)
+                .getPayload();
+    }
+
+    public SecretKey generateSignInKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
