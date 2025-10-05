f1e75e3a3c740cf8c5a2307f3fa6eeb806ea3a31
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-05 19:36:49 +0100

    security configuration

--- src/main/java/com/expensetracker/security/SecurityConfig.java
+++ src/main/java/com/expensetracker/security/SecurityConfig.java
@@ -5,14 +5,10 @@ import com.expensetracker.services.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.expensetracker.config.ApplicationConfig;
@@ -31,7 +27,7 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth ->
-                auth.requestMatchers("/api/tracker/login**","/api/tracker/register**")
+                auth.requestMatchers("/api/tracker/auth/authenticate**","/api/tracker/auth/register**")
                 .permitAll()
                 .anyRequest()
                 .authenticated())
@@ -43,15 +39,5 @@ public class SecurityConfig {
                         UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
     }
 }
