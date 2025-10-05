94d8ef67e88aba42b0626165a0b45e4377c04a61
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-05 23:45:17 +0100

    authentication request entity for its fields

--- src/main/java/com/expensetracker/dtos/requests/AuthenticationRequest.java
+++ src/main/java/com/expensetracker/dtos/requests/AuthenticationRequest.java
@@ -1,5 +1,6 @@
 package com.expensetracker.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 }
