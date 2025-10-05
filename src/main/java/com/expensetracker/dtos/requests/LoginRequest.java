43f6cc330cdb93afaabd9dfd769ac370620db2d5
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-05 19:36:38 +0100

    for user login request

--- src/main/java/com/expensetracker/dtos/requests/LoginRequest.java
+++ src/main/java/com/expensetracker/dtos/requests/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.expensetracker.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class LoginRequest {
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String password;
 }
