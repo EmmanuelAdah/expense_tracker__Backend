a0b030c1d4a1cbed2694e924c105fa7b4dcd0f41
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-06 22:35:05 +0100

    expense repository for manipulating the expense database

--- src/main/java/com/expensetracker/data/repositories/UserRepository.java
+++ src/main/java/com/expensetracker/data/repositories/UserRepository.java
@@ -13,7 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
 
-    Optional<User> findById(Long userId);
+    Optional<User> findById(long userId);
 
     Optional<User> findByEmail(String email);
 }
