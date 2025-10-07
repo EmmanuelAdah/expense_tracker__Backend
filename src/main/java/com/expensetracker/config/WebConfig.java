2eb196e75def194970cac81ecc114908f5e4e3bc
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-07 15:49:35 +0100

    CORS configurations for http request

--- src/main/java/com/expensetracker/config/WebConfig.java
+++ src/main/java/com/expensetracker/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:63343") // JS PORT
+                .allowedOrigins("http://localhost:63343", "/**") // JS PORTS
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
