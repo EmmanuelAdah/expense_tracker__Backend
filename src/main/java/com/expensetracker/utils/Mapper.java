52ab973bfcf6c972c86cabf059a6898856ff5cd4
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-08 19:42:08 +0100

    for mapping fields on request and response

--- src/main/java/com/expensetracker/utils/Mapper.java
+++ src/main/java/com/expensetracker/utils/Mapper.java
@@ -3,23 +3,11 @@ package com.expensetracker.utils;
 import com.expensetracker.data.models.Expense;
 import com.expensetracker.data.models.User;
 import com.expensetracker.dtos.requests.AddExpenseRequest;
-import com.expensetracker.dtos.requests.RegisterRequest;
 import com.expensetracker.dtos.response.ExpenseResponse;
 import com.expensetracker.dtos.response.UserResponse;
 import java.time.LocalDate;
-import static com.expensetracker.utils.PasswordEncoder.hashPassword;
 
 public class Mapper {
-
-    public static User mapUser(RegisterRequest request){
-        User user = new User();
-        user.setFirstname(request.getFirstname());
-        user.setLastname(request.getLastname());
-        user.setEmail(request.getEmail());
-        user.setUsername(request.getUsername());
-        user.setPassword(hashPassword(request.getPassword()));
-        return user;
-    }
 
     public static UserResponse map(User user){
         UserResponse response = new UserResponse();
@@ -32,7 +20,7 @@ public class Mapper {
     public static Expense mapExpense(AddExpenseRequest request) {
         Expense expense = new Expense();
         expense.setUserId(request.getUserId());
-        expense.setName(request.getName());
+        expense.setName(request.getName().toUpperCase());
         expense.setAmount(request.getAmount());
         expense.setCategory(request.getCategory());
         expense.setCreatedAt(LocalDate.now());
