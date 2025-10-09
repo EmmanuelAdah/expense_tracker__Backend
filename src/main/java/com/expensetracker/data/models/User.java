0383fa0c00584cb607872ad6117ce7e7e8aab2ba
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-09 22:57:49 +0100

    user entity for its attributes

--- src/main/java/com/expensetracker/data/models/User.java
+++ src/main/java/com/expensetracker/data/models/User.java
@@ -1,6 +1,7 @@
 package com.expensetracker.data.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
