b8397bdc5b02652856b10ea8ed5b05d912404cb6
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-09 22:58:44 +0100

    for performing CRUD operations on user data

--- src/main/java/com/expensetracker/data/repositories/UserRepository.java
+++ src/main/java/com/expensetracker/data/repositories/UserRepository.java
@@ -16,4 +16,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(long userId);
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
