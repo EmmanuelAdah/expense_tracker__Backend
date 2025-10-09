8d6147176edd705c098c63ae2b1b66ee47d4e7b3
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-09 22:59:02 +0100

    user service interface

--- src/main/java/com/expensetracker/services/UserService.java
+++ src/main/java/com/expensetracker/services/UserService.java
@@ -1,7 +1,6 @@
 package com.expensetracker.services;
 
 import com.expensetracker.dtos.requests.LoginRequest;
-import com.expensetracker.dtos.requests.RegisterRequest;
 import com.expensetracker.dtos.response.UserResponse;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -9,9 +8,9 @@ import java.util.List;
 @Service
 public interface UserService {
 
-    UserResponse registerUser(RegisterRequest request);
     UserResponse findById(long userId);
     UserResponse findByUsername(String username);
+    UserResponse findByEmail(String email);
     List<UserResponse> findAll();
     UserResponse userLogin(LoginRequest loginRequest);
     void deleteById(long id);
