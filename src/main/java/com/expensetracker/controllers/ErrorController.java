db887ad9edcd6bc5df1a5768c509374ec0bdd231
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-09 19:16:58 +0100

    error controller class

--- src/main/java/com/expensetracker/controllers/ErrorController.java
+++ src/main/java/com/expensetracker/controllers/ErrorController.java
@@ -1,5 +1,17 @@
 package com.expensetracker.controllers;
 
+import com.expensetracker.exceptions.InvalidLoginCredentialsException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.Map;
+
+@ControllerAdvice
 public class ErrorController {
 
+    @ExceptionHandler(InvalidLoginCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleException(InvalidLoginCredentialsException exception) {
+        Map<String, String> errorMessages = Map.of("message", exception.getMessage());
+        return ResponseEntity.badRequest().body(errorMessages);
+    }
 }
