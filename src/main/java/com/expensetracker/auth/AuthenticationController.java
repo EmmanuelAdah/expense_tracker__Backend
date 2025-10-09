8278cad2a1684d9cbccb4acb1ebb74258f854aed
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-09 19:18:27 +0100

    for user registration or login request for server response

--- src/main/java/com/expensetracker/auth/AuthenticationController.java
+++ src/main/java/com/expensetracker/auth/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.expensetracker.dtos.requests.AuthenticationRequest;
 import com.expensetracker.dtos.requests.RegisterRequest;
 import com.expensetracker.dtos.response.AuthenticationResponse;
 import com.expensetracker.services.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,13 +19,13 @@ public class AuthenticationController {
     private final AuthenticationService authService;
 
     @PostMapping("/register/user")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<AuthenticationResponse> register(@Valid
             @RequestBody RegisterRequest request){
         return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping("/authenticate/user")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public ResponseEntity<AuthenticationResponse> authenticate(@Valid
             @RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authService.authenticate(request));
     }
