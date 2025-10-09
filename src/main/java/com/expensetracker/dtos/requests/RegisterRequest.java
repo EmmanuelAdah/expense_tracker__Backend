fc9907f63c68ea45973c3d1739ffd6910a50465c
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-09 23:00:54 +0100

    for registration request fields

--- src/main/java/com/expensetracker/dtos/requests/RegisterRequest.java
+++ src/main/java/com/expensetracker/dtos/requests/RegisterRequest.java
@@ -1,6 +1,8 @@
 package com.expensetracker.dtos.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,17 +15,25 @@ import lombok.NoArgsConstructor;
 public class RegisterRequest {
 
     @NotBlank(message = "firstname")
+    @Size(min = 3, max = 30, message = "first name must be between {min} and {max} long")
+    @Pattern(regexp = "^\\w+$")
     private String firstname;
 
     @NotBlank(message = "lastname")
+    @Size(min = 3, max = 30, message = "last name must be between {min} and {max} long")
+    @Pattern(regexp = "^\\w+$")
     private String lastname;
 
     @NotBlank(message = "email")
+    @Size(min = 10, max = 30, message = "email must be between {min} and {max} long")
+//    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n")
     private String email;
 
     @NotBlank(message = "username")
+    @Size(min = 4, max = 30, message = "username must be between {min} and {max} long")
     private String username;
 
     @NotBlank(message = "password")
+    @Size(min = 6, max = 100, message = "password must be between {min} and {max} long")
     private String password;
 }
