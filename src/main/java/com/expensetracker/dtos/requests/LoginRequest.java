f8622862ece932629f5807397342edaddd59993c
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-10 22:07:21 +0100

    for user login request

--- src/main/java/com/expensetracker/dtos/requests/LoginRequest.java
+++ src/main/java/com/expensetracker/dtos/requests/LoginRequest.java
@@ -1,15 +1,18 @@
 package com.expensetracker.dtos.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class LoginRequest {
-    @NotBlank
+    @NotBlank(message = "username is mandatory")
+    @NotEmpty(message = "username is mandatory")
     private String username;
 
-    @NotBlank
+    @NotBlank(message = "password is mandatory")
+    @NotEmpty(message = "password is mandatory")
     private String password;
 }
