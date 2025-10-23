3bd80d37cb239d0e0ab8d398235f727549f518ee
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-23 18:57:43 +0100

    users repository for data access and manipulation

--- src/main/java/com/expensetracker/data/repositories/UserRepository.java
+++ src/main/java/com/expensetracker/data/repositories/UserRepository.java
@@ -1,21 +1,34 @@
 package com.expensetracker.data.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import com.expensetracker.data.models.User;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
     Optional<User> findByUsername(String username);
 
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.username = :username")
     void deleteByUsername(String username);
 
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.username = :username")
     boolean existsByUsername(String username);
 
+    @Query("SELECT u FROM User u WHERE u.userId = :userId")
     Optional<User> findById(long userId);
 
+    @Query("SELECT u FROM User u WHERE u.email = :email")
     Optional<User> findByEmail(String email);
 
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
     boolean existsByEmail(String email);
 }
