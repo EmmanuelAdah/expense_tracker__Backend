1a0c865048923f63ec10035893442e5ccb2e7eb4
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-25 23:30:47 +0100

    expense service implementation test

--- src/test/java/com/expensetracker/services/ExpenseServiceImplTest.java
+++ src/test/java/com/expensetracker/services/ExpenseServiceImplTest.java
@@ -6,28 +6,32 @@ import com.expensetracker.dtos.requests.RegistrationRequest;
 import com.expensetracker.dtos.response.UserResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
 import java.time.LocalDate;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ExpenseServiceImplTest {
 
-    @Mock
+    @Autowired
     public UserServiceImpl userServiceImpl;
 
-    @MockitoBean
+    @Autowired
     private AuthenticationService authenticationService;
 
-    @InjectMocks
+    @Autowired
     private ExpenseServiceImpl expenseServiceImpl;
-
 
     @BeforeEach
     void setUp() {
         expenseServiceImpl.deleteAll();
+    }
+
+    @BeforeEach
+    void setup() {
+        userServiceImpl.deleteAll();
     }
 
     @Test
@@ -35,24 +39,25 @@ class ExpenseServiceImplTest {
         RegistrationRequest request = new RegistrationRequest();
         request.setFirstname("firstname");
         request.setLastname("lastname");
-        request.setEmail("email");
+        request.setEmail("email@gmail.com");
         request.setUsername("username");
         request.setPassword("password");
 
         authenticationService.register(request);
+
         UserResponse response = userServiceImpl.findByUsername("username");
 
         AddExpenseRequest request1 = new AddExpenseRequest();
-        request1.setUserId(response.getUserId());
         request1.setName("Milk");
         request1.setAmount(1000);
         request1.setCategory("Food");
         request1.setType(Type.DIRECT);
         request1.setCreatedAt(LocalDate.now());
         request1.setDueDate(LocalDate.now().plusMonths(1));
-        expenseServiceImpl.saveExpense(request1);
-//        assertThat(expenseServiceImpl.findByUserId(response.getUserId()).size())
-//                .isEqualTo(1);
+        expenseServiceImpl.saveExpense(request1, response.getUsername());
+
+        assertThat(expenseServiceImpl.findByUserId(response.getUserId()).size())
+                .isEqualTo(1);
     }
 
 }
