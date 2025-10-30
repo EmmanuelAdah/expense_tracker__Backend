5057b766b531848b4ae042ca65824649a62a07b0
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-30 20:09:05 +0100

    for field validations

--- src/main/java/com/expensetracker/utils/Validator.java
+++ src/main/java/com/expensetracker/utils/Validator.java
@@ -1,4 +1,9 @@
 package com.expensetracker.utils;
 
 public class Validator {
+
+    public static boolean isValidEmail(String email){
+        return email.matches("^(?=.{3,30}@)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,3}$"
+        );
+    }
 }
