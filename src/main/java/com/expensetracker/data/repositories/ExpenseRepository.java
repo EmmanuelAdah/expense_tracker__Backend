4ab034fd06324b6949a97c8d19c5cb543b973395
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-01 19:48:48 +0100

    expense repository for database manipulation

--- src/main/java/com/expensetracker/data/repositories/ExpenseRepository.java
+++ src/main/java/com/expensetracker/data/repositories/ExpenseRepository.java
@@ -2,9 +2,21 @@ package com.expensetracker.data.repositories;
 
 import com.expensetracker.data.models.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
+    List<Expense> findByUserId(long userId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Expense e WHERE e.id = :id")
+    void deleteExpenseById(@Param("id") long id);
 }
