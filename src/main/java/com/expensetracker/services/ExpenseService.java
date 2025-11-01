8c0d05e2c3024917635f8487d9e17e56c20c0016
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-01 19:48:22 +0100

    expense service interface

--- src/main/java/com/expensetracker/services/ExpenseService.java
+++ src/main/java/com/expensetracker/services/ExpenseService.java
@@ -2,6 +2,7 @@ package com.expensetracker.services;
 
 import com.expensetracker.dtos.requests.AddExpenseRequest;
 import com.expensetracker.dtos.response.ExpenseResponse;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -12,6 +13,7 @@ public interface ExpenseService {
     ExpenseResponse findById(Long expenseId);
     List<ExpenseResponse> findByUserId(long userId);
     List<ExpenseResponse> findAll();
+    void deleteByExpenseId(@Valid Long id);
     void deleteAllByUserId(Long userId);
     void deleteAll();
 }
