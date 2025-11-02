4fc2e8a7385a7ea8c74f2d8ef10e6a3c764cb433
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-02 22:42:11 +0100

    type of expense

--- src/main/java/com/expensetracker/data/models/Type.java
+++ src/main/java/com/expensetracker/data/models/Type.java
@@ -1,15 +1,8 @@
 package com.expensetracker.data.models;
 
+import lombok.Getter;
+
+@Getter
 public enum Type {
-    FIXED, VARIABLE, CAPITAL, DIRECT, INDIRECT, OPERATIONAL;
-
-    private Type type;
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
+    FIXED, VARIABLE, CAPITAL, DIRECT, INDIRECT, OPERATIONAL
 }
