9c6fd239d15816c246ba8ded2d877a466729e8cc
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-03 19:13:19 +0100

    user service interface

--- src/main/java/com/expensetracker/services/UserService.java
+++ src/main/java/com/expensetracker/services/UserService.java
@@ -4,16 +4,17 @@ import com.expensetracker.data.models.User;
 import com.expensetracker.dtos.response.UserResponse;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface UserService {
 
     User save(User user);
-    UserResponse findById(long userId);
+    UserResponse findById(UUID userId);
     User findByUsername(String username);
     UserResponse findByEmail(String email);
     List<UserResponse> findAll();
-    void deleteById(long id);
+    void deleteById(UUID id);
     void deleteByUsername(String username);
     void deleteAll();
     Double setIncome(String username, double income);
