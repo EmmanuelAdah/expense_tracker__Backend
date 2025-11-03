c3c3e64dce9bc66640b885436461ecd8d89ac46e
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-03 19:12:58 +0100

    user response entity

--- src/main/java/com/expensetracker/dtos/response/UserResponse.java
+++ src/main/java/com/expensetracker/dtos/response/UserResponse.java
@@ -3,10 +3,12 @@ package com.expensetracker.dtos.response;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Data
 @Component
 public class UserResponse {
-    private long userId;
+    private UUID userId;
     private String email;
     private String username;
     private double balance;
