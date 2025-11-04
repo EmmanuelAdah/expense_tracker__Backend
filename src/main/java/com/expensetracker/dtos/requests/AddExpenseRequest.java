8847f10bccc471577fb8115c1edc69b9d85866e9
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-11-04 20:20:21 +0100

    add expense request entity

--- src/main/java/com/expensetracker/dtos/requests/AddExpenseRequest.java
+++ src/main/java/com/expensetracker/dtos/requests/AddExpenseRequest.java
@@ -1,13 +1,11 @@
 package com.expensetracker.dtos.requests;
 
 
-import com.expensetracker.data.models.Type;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.stereotype.Component;
-import java.time.LocalDate;
 
 @Data
 @Component
@@ -18,17 +16,13 @@ public class AddExpenseRequest {
     private String name;
 
     @NotNull
-    @Size(message = "amount must be greater than 0")
     private long amount;
+
+    @NotNull
+    @Size(min = 3, max = 100, message = "Type must be between {min} and {max} letters long")
+    private String type;
 
     @NotNull
     @Size(min = 3, max = 100, message = "Category must be between {min} and {max} letters long")
     private String category;
-
-    @NotNull
-    @Size(min = 1, max = 100, message = "Type must be between {min} and {max} letters long")
-    private Type type;
-
-    @NotNull
-    private LocalDate createdAt;
 }
